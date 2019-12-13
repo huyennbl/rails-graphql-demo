0b0380b5d1034a57508b9c9ee0427e3752c98d54
Huyên NgBL <huyenblnguyen@kms-technology.com>
2019-12-13 13:00:29 +0700

    add books query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,9 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :books, [Types::BookType], null: false
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def books
+      Book.all
     end
   end
 end
