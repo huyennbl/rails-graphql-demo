81936235f5d083bc92debc03c60aca00a5bcbaf4
Huyên NgBL <huyenblnguyen@kms-technology.com>
2019-12-13 13:55:06 +0700

    add function for create_user mutation

--- app/graphql/mutations/create_book.rb
+++ app/graphql/mutations/create_book.rb
@@ -1,14 +1,28 @@
 module Mutations
   class CreateBook < BaseMutation
     # TODO: define return fields
-    # field :post, Types::PostType, null: false
-
+    field :book, Types::BookType, null: false
+    field :errors, [String], null: false
+    
     # TODO: define arguments
-    # argument :name, String, required: true
+    argument :title, String, required: true
 
     # TODO: define resolve method
-    # def resolve(name:)
-    #   { post: ... }
-    # end
+    def resolve(title:)
+      puts "into create_book resolver #{title}"
+      book = Book.new(title: title)
+      if(book.save)
+        {
+          book: book,
+          errors: []
+        }
+      else
+        {
+          book: nil,
+          errors: [book.errors.full_messages]
+        }
+      end
+
+    end
   end
 end
