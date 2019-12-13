f1de56c4a08c2a4ae9d45754a12ee4db8773366d
Huyên NgBL <huyenblnguyen@kms-technology.com>
2019-12-13 13:02:41 +0700

    add authors query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,9 +1,14 @@
 module Types
   class QueryType < Types::BaseObject
     field :books, [Types::BookType], null: false
-
     def books
       Book.all
+    end
+
+
+    field :authors, [Types::AuthorType], null: false
+    def authors
+      Author.all
     end
   end
 end
